fa4efda951891e7562aa7de4fcf882ae25004108
Jonnix12 <jonharel48@gmail.com>
2023-04-11 12:33:15 +0300

    Add spline editior

---
+++ Assets/Scripts/Testing/TestSpline.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class TestSpline : MonoBehaviour
+{
+    [SerializeField] private SpriteShapeController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
