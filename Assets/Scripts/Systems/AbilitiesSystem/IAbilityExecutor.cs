f39916b5e40bb8523b962eee963c35a39aa51c93
Jonnix12 <jonharel48@gmail.com>
2023-06-18 15:21:31 +0300

    Update

---
+++ Assets/Scripts/Systems/AbilitiesSystem/IAbilityExecutor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Tzipory.AbilitiesSystem.AbilityConfigSystem;
+using Tzipory.EntitySystem.EntityComponents;
+using Tzipory.EntitySystem.StatusSystem;
+
+namespace Tzipory.AbilitiesSystem
+{
+    public interface IAbilityExecutor
+    {
+        public AbilityExecuteType AbilityExecuteType { get; }
+        public IEntityTargetingComponent Caster { get; }
+        
+        protected List<BaseStatusEffect> StatusEffects { get; }
+        
+        public void Execute(IEntityTargetAbleComponent target);
+    }
+}
